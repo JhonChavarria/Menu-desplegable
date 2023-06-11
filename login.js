714e384db88a3b27ed8f75ffbbf8c479d6926f6d
JhonChavarria <jhonmarlonchavarria@gmail.com>
2023-06-11 18:32:04 -0500

    cambio

--- login.js
+++ login.js
@@ -1,7 +1,13 @@
-const nombre = document.getElementById("ingresar")
 
 
-form.addEventListener("click", e => {
-    e.preventDefault()
-    window.location="index.html"
-})
+function ingresar() {
+
+    const email = document.getElementById("email").value
+    const nombre = document.getElementById("nombre").value
+
+
+    if (email) {
+        window.location = "index.html"
+    }
+
+}
