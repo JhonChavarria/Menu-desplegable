3ceadd5296fea98cab6368f72d5a2463f481971f
JhonChavarria <jhonmarlonchavarria@gmail.com>
2023-06-15 23:02:45 -0500

    Nuevos campos

--- login.js
+++ login.js
@@ -1,13 +1,40 @@
 
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
 
-function ingresar() {
+form.addEventListener("submit", e => {
+    e.preventDefault()
 
-    const email = document.getElementById("email").value
-    const nombre = document.getElementById("nombre").value
+    //EXPRESIONES REGULARES PARA VALIDAR EL EMAIL: 
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    var letters = /^[A-Za-z]+$/;
 
 
-    if (email) {
-        window.location = "index.html"
+
+
+    if (email.value == "" && pass.value == "") {
+        // alert("LLenar los campos")
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Llenar los campos',
+        })
+
+        
     }
 
+
+
+    else if (!regexEmail.test(email.value)) {
+
+        alert("El email no es válido")
+
+    }
+    else if (!letters.test(pass.value)) {
+        alert("La contraseña no es válida")
+    } else {
+        window.location = "index.html"
+    }
 }
+)
